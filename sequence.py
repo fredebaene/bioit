78cd6fc1be246720aaa7d7031fa57d47a32f3202
Frederick De Baene <frederickdebaene@MacBook-Pro-van-Frederick.local>
2021-05-05 14:04:32 +0200

    Add Frequency Distribution for Monomers

--- sequence.py
+++ sequence.py
@@ -2,6 +2,34 @@
 # ------------------------------------------------------------------------------
 class Sequence(object):
 
+    monomers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+
     def __init__(self, sequence, label=None):
         self.sequence = sequence
         self.label = label
+
+    @property
+    def len(self):
+        """The length of the polymer expressed as the number of monomers."""
+        return len(self.sequence)
+
+    def monomer_frequencies(self):
+        """Returns a frequency distribution of the monomers as a dictionary."""
+        frequency_distribution = {}
+        for i in self.monomers:
+            frequency_distribution[i] = 0
+        for i in self.sequence:
+            frequency_distribution[i] += 1
+        return frequency_distribution
+
+# DNA CLASS
+# ------------------------------------------------------------------------------
+class DNA(Sequence):
+
+    monomers = 'ACGT'
+
+# RNA CLASS
+# ------------------------------------------------------------------------------
+class RNA(Sequence):
+
+    monomers = 'ACGU'
